96388c37df4743cfb2061038c86aad6621d8da5d
Samuel Nitsche <mail@samynitsche.de>
2018-11-02 12:02:44 +0100

    Refactored code

--- bin/both/backup.py
+++ bin/both/backup.py
@@ -1,38 +1,73 @@
-from bin.both.fs_local import Backup
 from bin.both.dbcon import dbmanager
 from crontab import CronTab
 from datetime import datetime
 import time
 
-db = dbmanager()
+db = dbmanager(True)
 threshold = 10
 
+def startBackup(task):
+    updateTaskState(task, 'running')
+
+def finishBackup(task):
+    db.log(task, '=============FINIHSED=============')
+    updateLastRunDate(task)
+    updateTaskState(task, 'waiting')
+
+def updateLastRunDate(task):
+    date = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    db.query('UPDATE \'143_tasks\' SET \'last_run\' = \''+date+'\' WHERE id = '+str(task))
+
+def updateTaskState(task, state):
+    db.query('UPDATE \'143_tasks\' SET \'state\' = \''+state+'\' WHERE id = '+str(task))
+
 while True:
-    query = db.query('SELECT c.path, d.path, a.schedule, a.id, a.last_run FROM \'143_tasks\' AS a '
+    query = db.query('SELECT c.path AS source, '
+                     'd.path AS dest, '
+                     'a.schedule, '
+                     'a.id, '
+                     'a.last_run, '
+                     'c.system AS source_fs, '
+                     'd.system AS dest_fs, '
+                     'c.host AS host, '
+                     'c.username AS user, '
+                     'c.password AS password, '
+                     'a.state '
+                     'FROM \'143_tasks\' AS a '
                      'JOIN \'143_backups\' AS b ON a.backupid = b.id '
                      'JOIN \'143_pool\' AS c ON b.pool_src = c.id '
                      'JOIN \'143_pool\' AS d ON b.pool_dst = d.id')
 
     tasks = query.fetchall()
     for task in tasks:
-        src = task[0]
-        dst = task[1]
-        schedule = task[2]
-        task_id = task[3]
-        last_run = task[4]
 
-        if not last_run == None:
-            schedule = CronTab(schedule)
-            diff = schedule.next()
-            if diff < threshold:
-                backup = Backup(src, dst, task_id)
-                backup.backup()
-                print('Backup for task ' + str(task_id) + ' created')
+        # Import correct script for filesystem
+        if task['source_fs'] == 'ftp':
+            from bin.both.fs_ftp import Backup
+        elif task['source_fs'] == 'sftp':
+            from bin.both.fs_sftp import Backup
         else:
-            backup = Backup(src, dst, task_id)
-            backup.backup()
-            print('Backup for task ' + str(task_id) + ' created')
+            from bin.both.fs_local import Backup
 
-    # print('Backups created')
+        if task['state'] is not 'running':
+            if task['last_run'] is not None:
+                schedule = CronTab(task['schedule'])
+                diff = schedule.next()
+                if diff < threshold:
+                    startBackup(task['id'])
+                    backup = Backup(task)
+                    backup.backup()
+                    finishBackup(task['id'])
+                    print('Backup for task ' + str(task['id']) + ' created')
+
+            # Start backup immediately if never ran before
+            else:
+                startBackup(task['id'])
+                backup = Backup(task)
+                backup.backup()
+                finishBackup(task['id'])
+                print('Backup for task ' + str(task['id']) + ' created')
+        else:
+            print('Backup already running')
 
     time.sleep(10)
--- bin/both/fs_local.py
+++ bin/both/fs_local.py
@@ -1,42 +1,21 @@
 import os
 import zipfile
 from bin.both.dbcon import dbmanager
-from datetime import datetime
 
 class Backup:
-    def __init__(self, source, dest, task):
+    def __init__(self, task):
         self.db = dbmanager()
-        self.source = source
-        self.dest = dest
         self.task = task
 
     def backup(self):
-        self.updateTaskState('running')
-        zipf = zipfile.ZipFile(self.dest, "w")
-        os.chdir(self.source)
+        zipf = zipfile.ZipFile(self.task['dest'], "w")
+        os.chdir(self.task['source'])
         for subdir, dirs, files in os.walk("."):
             for dir in dirs:
                 path = os.path.join(subdir, dir)
-                self.db.log(self.task, "Created backup of folder " + dir)
+                self.db.log(self.task['id'], "Created backup of folder " + dir)
                 zipf.write(path)
-                # print('Backed up folder '+dir)
 
             for file in files:
-                self.db.log(self.task, "Created backup of file " + file)
+                self.db.log(self.task['id'], "Created backup of file " + file)
                 zipf.write(os.path.join(subdir, file))
-                # print('Backed up file ' + file)
-
-        self.finishBackup()
-        print("All done!")
-
-    def finishBackup(self):
-        self.db.log(self.task, '=============FINIHSED=============')
-        self.updateLastRunDate()
-        self.updateTaskState('waiting')
-
-    def updateLastRunDate(self):
-        date = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-        self.db.query('UPDATE \'143_tasks\' SET \'last_run\' = \''+date+'\' WHERE id = '+str(self.task))
-
-    def updateTaskState(self, state):
-        self.db.query('UPDATE \'143_tasks\' SET \'state\' = \''+state+'\' WHERE id = '+str(self.task))
