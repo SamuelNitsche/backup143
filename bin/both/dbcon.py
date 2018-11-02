e2fe5ac6788462c70754b0f3d7c8e42105136f6b
Samuel Nitsche <mail@samynitsche.de>
2018-11-02 12:02:03 +0100

    Added possibility to fetch an assoc array

--- bin/both/dbcon.py
+++ bin/both/dbcon.py
@@ -4,14 +4,15 @@ from datetime import datetime
 from bin.both.log import LogginSystem as logsys
 
 class dbmanager():
-    def __init__(self):
+    def __init__(self, dict=False):
         try:
             script_dir = os.path.dirname(__file__)
             rel_path = "../../backup143.db"
             database_file_path = os.path.join(script_dir, rel_path)
             self.conn = sqlite3.connect(database_file_path, timeout=30)
+            if dict:
+                self.conn.row_factory = self.dict_factory
             self.cur = self.conn.cursor()
-
             log = logsys('db')
             log.write('Successfully connected to backup143.db')
         except sqlite3.Error as e:
@@ -54,6 +55,12 @@ class dbmanager():
         date = datetime.now()
         query = f'INSERT INTO \'143_tasklog\' (date, taskid, value) VALUES (\'{date}\', {task}, \'{value}\')'
         self.query(query)
+
+    def dict_factory(self, cursor, row):
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
 
     def __exit__(self):
         log = logsys('db')
