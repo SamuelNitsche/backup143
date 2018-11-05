b8ac524dd88a31bdf29ac5034c6b965cf35c0a87
Samuel Nitsche <mail@samynitsche.de>
2018-11-05 20:11:41 +0100

    Implemented tar.gz compression

--- bin/both/backup/fs_local.py
+++ bin/both/backup/fs_local.py
@@ -1,5 +1,6 @@
 import os
 import zipfile
+import tarfile
 import shutil
 from bin.both.dbcon import dbmanager
 from datetime import datetime
@@ -30,13 +31,6 @@ class Backup:
 
             # Walk directory
             for subdir, dirs, files in os.walk("."):
-                # if self.task['type'] == 'full':
-                #     for dir in dirs:
-                #         path = os.path.join(subdir, dir)
-                #         self.db.log(self.task['id'], "Created backup of folder " + dir)
-                #         # Write folder to zip file
-                #         zipf.write(path)
-
                 for file in files:
                     if self.task['type'] == 'incremental':
                         # Check if file hash has changed (only for incremental backup)
@@ -91,12 +85,83 @@ class Backup:
                     os.mkdir(os.path.join(destination, dir))
 
                 for file in files:
-                    # Check if file hash has changed (only for incremental backup)
-                    if filechanged(self.task, os.path.join(self.task['source'], subdir), file, date,
-                                   self.task['last_run']):
-                        self.db.log(self.task['id'], "Created backup of file " + file)
-                        # Copy file to new directory (preserve metadata)
-                        shutil.copy2(os.path.join(subdir, file), os.path.join(destination, subdir))
+                    if self.task['type'] == 'incremental':
+                        # Check if file hash has changed (only for incremental backup)
+                        if filechanged(self.task, os.path.join(self.task['source'], subdir), file, date,
+                                       self.task['last_run']):
+                            print('File ' + file + ' has changed. Backing up.')
+                            self.db.log(self.task['id'], "Created backup of file " + file)
+                            # Copy file to new directory (preserve metadata)
+                            shutil.copy2(os.path.join(subdir, file), os.path.join(destination, subdir))
+
+                    elif self.task['type'] == 'differential':
+                        # Check if file hash has changed (only for incremental backup)
+                        if filechanged(self.task, os.path.join(self.task['source'], subdir), file, date,
+                                       self.task['last_full_run']):
+                            print('File ' + file + ' has changed. Backing up.')
+                            self.db.log(self.task['id'], "Created backup of file " + file)
+                            # Copy file to new directory (preserve metadata)
+                            shutil.copy2(os.path.join(subdir, file), os.path.join(destination, subdir))
+
+                    elif self.task['type'] == 'full':
+                        # Check if file hash has changed (only for incremental backup)
+                        if filechanged(self.task, os.path.join(self.task['source'], subdir), file, date,
+                                       self.task['last_run']):
+                            print('File ' + file + ' has changed. Backing up.')
+                            self.db.log(self.task['id'], "Created backup of file " + file)
+                            # Copy file to new directory (preserve metadata)
+                            shutil.copy2(os.path.join(subdir, file), os.path.join(destination, subdir))
+
+            data = {
+                'task': self.task['id'],
+                'date': date,
+                'status': 'ok',
+                'path': destination
+            }
+
+        elif self.task['compression'] == 'tar':
+            # Check if destination folder exists
+            if not os.path.isdir(self.task['dest']):
+                raise Exception('Destination directory does not exist')
+
+            # Calculate name for backup file
+            date = str(datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+            destination = self.task['dest'] + os.sep + self.task['type'] + '_' + date + '.tar.gz'
+
+            # Initialize tar file for backup
+            tarf = tarfile.open(destination, "w:gz")
+
+            # Walk directory
+            for subdir, dirs, files in os.walk("."):
+                for file in files:
+                    if self.task['type'] == 'incremental':
+                        # Check if file hash has changed (only for incremental backup)
+                        if filechanged(self.task, os.path.join(self.task['source'], subdir), file, date,
+                                       self.task['last_run']):
+                            print('File ' + file + ' has changed. Backing up.')
+                            self.db.log(self.task['id'], "Created backup of file " + file)
+                            # Write file to tar
+                            tarf.add(os.path.join(subdir, file))
+
+                    elif self.task['type'] == 'differential':
+                        # Check if file hash has changed (only for incremental backup)
+                        if filechanged(self.task, os.path.join(self.task['source'], subdir), file, date,
+                                       self.task['last_full_run']):
+                            print('File ' + file + ' has changed. Backing up.')
+                            self.db.log(self.task['id'], "Created backup of file " + file)
+                            # Write file to tar
+                            tarf.add(os.path.join(subdir, file))
+
+                    elif self.task['type'] == 'full':
+                        # Check if file hash has changed (only for incremental backup)
+                        if filechanged(self.task, os.path.join(self.task['source'], subdir), file, date,
+                                       self.task['last_run']):
+                            print('File ' + file + ' has changed. Backing up.')
+                            self.db.log(self.task['id'], "Created backup of file " + file)
+                            # Write file to tar
+                            tarf.add(os.path.join(subdir, file))
+
+            tarf.close()
 
             data = {
                 'task': self.task['id'],
